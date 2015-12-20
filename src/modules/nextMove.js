a3c7168d13a1d95d644e2770524897d75c6247c0
angus-c <anguscroll@gmail.com>
2015-12-20 14:20:58 -0800

    caching hell WIP

--- src/modules/nextMove.js
+++ src/modules/nextMove.js
@@ -1,19 +1,30 @@
 import game from '../data/game';
 import profiler from '../utilities/profiler';
 
-let movesLookup;
+let bestMoveLookup;
+let possibleMovesLookup;
 
 let bestScoreSoFar = -100, originalPlayer, requestedDepth;
 
 const nextMove = (player, position, depth) => {
-  movesLookup = {
-    north: {},
-    south: {}
-  };
+  bestMoveLookup = {};
+  for (let thisDepth = 0; thisDepth <= depth; thisDepth++) {
+    bestMoveLookup[thisDepth] = {
+      north: {}, south: {}
+    };
+  }
+  possibleMovesLookup = {north: {}, south: {}};
   originalPlayer = player;
   requestedDepth = depth;
   const timeForNextMove = profiler('timeForNextMove');
-  const move = getSortedMoves(player, position, depth)[0];
+  let move;
+  move = bestMoveLookup[depth][player.name][position.toStr];
+  if (move) {
+    timeForNextMove.stop();
+    return move;
+  }
+  move = getSortedMoves(player, position, depth)[0];
+  bestMoveLookup[depth][player.name][position.toStr] = move;
   timeForNextMove.stop();
   return move;
 };
@@ -28,7 +39,7 @@ const getSortedMoves = (player, position, depth) => {
 };
 
 const getPossibleMoves = (player, position) => {
-  const savedMoves = movesLookup[player.name][position.toStr];
+  const savedMoves = possibleMovesLookup[player.name][position.toStr];
   if (savedMoves) {
     return savedMoves;
   }
@@ -40,23 +51,26 @@ const getPossibleMoves = (player, position) => {
     return moves;
   }, []);
 
-  movesLookup[player.name][position.toStr] = moves;
+  possibleMovesLookup[player.name][position.toStr] = moves;
   return moves;
 };
 
 const deepScore = (move, position, depth) => {
   let counterScore = 0;
   if (depth > 0) {
-    const bestMove = getSortedMoves(
+    const savedBestMove = bestMoveLookup[depth - 1][move.player.name][position.toStr];
+    const bestMove = savedBestMove || getSortedMoves(
       game.getOtherPlayer(move.player),
       simulateMove(move, position),
       depth - 1
     )[0];
     move.bestReply = bestMove;
+    if (!savedBestMove) {
+      bestMoveLookup[depth - 1][move.player.name][position.toStr] = bestMove;
+    }
     counterScore = bestMove ? bestMove.score : 0;
   }
   const thisScore = score(move, depth) - counterScore;
-  (thisScore > bestScoreSoFar) && (bestScoreSoFar = thisScore);
   return thisScore;
 };
 
