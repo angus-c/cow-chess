307b84833fc27517a8cf79acb69b238133eb1aa3
angus-c <anguscroll@gmail.com>
2015-12-28 10:52:44 -0800

    "fun" with move cropping

--- src/modules/nextMove.js
+++ src/modules/nextMove.js
@@ -5,9 +5,12 @@ import Pawn from './pieces/Pawn';
 
 let movesLookup;
 
-let bestScoreSoFar = -100, originalPlayer, requestedDepth;
+let bestScoreSoFar, originalPlayer, requestedDepth;
+// store in closure scope because passing it through recursion is hella messy
+let currentRecursionScore;
 
 const nextMove = (player, position, depth) => {
+  bestScoreSoFar = Number.NEGATIVE_INFINITY;
   movesLookup = {
     north: {},
     south: {}
@@ -16,9 +19,9 @@ const nextMove = (player, position, depth) => {
   requestedDepth = depth;
   const moveTime = profiler('moveTime');
   const firstPass = getSortedMoves(player, position, 1);
-  const shortlist = firstPass.slice(0, 5);
+  const shortlist = firstPass.slice(0, 3);
   const secondPass = getSortedMoves(player, position, 3, shortlist);
-  const secondShortlist = secondPass.slice(0, 3);
+  const secondShortlist = secondPass.slice(0, 2);
   const move = getSortedMoves(player, position, depth, secondShortlist)[0];
   move.time = moveTime.getElapsed();
   return move;
@@ -27,7 +30,17 @@ const nextMove = (player, position, depth) => {
 const getSortedMoves = (player, position, depth, possibleMoves) => {
   !possibleMoves && (possibleMoves = getPossibleMoves(player, position));
   const scoredMoves = possibleMoves.map(move => {
-    move.score = deepScore(move, position, depth);
+    // excuse the side effects
+    if (depth == requestedDepth) {
+      // initialize score for this move drill-down
+      currentRecursionScore = 0;
+    }
+    const score = deepScore(move, position, depth);
+    if (depth == requestedDepth) {
+      // update top level best score
+      (score > bestScoreSoFar) && (bestScoreSoFar = score);
+    }
+    move.score = score;
     return move;
   });
   return scoredMoves.sort(
@@ -53,19 +66,30 @@ const getPossibleMoves = (player, position) => {
 
 const deepScore = (move, position, depth) => {
   let counterScore = 0, sortedReplies;
-  if (depth > 0) {
-    // TODO: use cached value?
-    sortedReplies = getSortedMoves(
-      game.getOtherPlayer(move.player),
-      simulateMove(move, position),
-      depth - 1
-    );
-    move.numberOfReplies = sortedReplies.length;
-    move.bestReply = sortedReplies[0];
-    counterScore = sortedReplies[0] ? sortedReplies[0].score : 0;
+  // TODO: cache this?
+  const shallowScore = score(move, position, depth);
+  if (move.player === originalPlayer) {
+    currentRecursionScore += shallowScore;
+  } else {
+    currentRecursionScore -= shallowScore;
   }
-  const thisScore = score(move, position, depth) - counterScore;
-  (thisScore > bestScoreSoFar) && (bestScoreSoFar = thisScore);
+  if (depth > 0) {
+    const giveIn =
+      (move.player == originalPlayer) &&
+      (depth != requestedDepth) &&
+      (currentRecursionScore < bestScoreSoFar);
+    if (!giveIn) {
+      sortedReplies = getSortedMoves(
+        game.getOtherPlayer(move.player),
+        simulateMove(move, position),
+        depth - 1
+      );
+      move.numberOfReplies = sortedReplies.length;
+      move.bestReply = sortedReplies[0];
+      counterScore = sortedReplies[0] ? sortedReplies[0].score : 0;
+    }
+  }
+  const thisScore = shallowScore - counterScore;
   return thisScore;
 };
 
